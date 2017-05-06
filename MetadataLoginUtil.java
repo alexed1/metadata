6c48ceee85063e30bc04b4b306bac74ac7895915
Alex Edelstein <alex@edelstein.org>
2017-05-06 16:14:39 +0000

    first commit

---
+++ MetadataLoginUtil.java
@@ -0,0 +1,38 @@
+import com.sforce.soap.enterprise.EnterpriseConnection;
+import com.sforce.soap.enterprise.LoginResult;
+import com.sforce.soap.metadata.MetadataConnection;
+import com.sforce.ws.ConnectionException;
+import com.sforce.ws.ConnectorConfig;
+
+ /**
+ * Login utility.
+ */
+
+
+ public class MetadataLoginUtil {
+    public static MetadataConnection login() throws ConnectionException {
+        final String USERNAME = "alexsfdev@edelstein.org";
+        // This is only a sample. Hard coding passwords in source files is a bad practice.
+        final String PASSWORD = "Foobarhead1!HMgSshV40fgwx2ROyJKj5CDwq";
+        final String URL = "https://login.salesforce.com/services/Soap/c/39.0";
+        final LoginResult loginResult = loginToSalesforce(USERNAME, PASSWORD, URL);
+        return createMetadataConnection(loginResult);
+    }
+    private static MetadataConnection createMetadataConnection(
+            final LoginResult loginResult) throws ConnectionException {
+        final ConnectorConfig config = new ConnectorConfig();
+        config.setServiceEndpoint(loginResult.getMetadataServerUrl());
+        config.setSessionId(loginResult.getSessionId());
+        return new MetadataConnection(config);
+    }
+    private static LoginResult loginToSalesforce(
+            final String username,
+            final String password,
+            final String loginUrl) throws ConnectionException {
+        final ConnectorConfig config = new ConnectorConfig();
+        config.setAuthEndpoint(loginUrl);
+        config.setServiceEndpoint(loginUrl);
+        config.setManualLogin(true);
+        return (new EnterpriseConnection(config)).login(username, password);
+    }
+}
